edb713a28ad5b33e80f83ba5675bd5991417a760
nishanthi2206 <nishanthi.rn95@gmail.com>
2024-11-26 17:08:15 +0530

    resume in html

---
+++ 26.11.2024/main.js
@@ -0,0 +1,88 @@
+let resume =
+{
+    name: "nishanthi.m",
+    mobile: 7200462155,
+    email: "nishanthi.95@gmail.com",
+
+    education_details:
+        [
+            {
+                course_name: "SSLC",
+                institute_name: "amala_convent_hr_sec_school",
+                year: 2015,
+                percentage: 90,
+
+            },
+            {
+                course_name: "HSC",
+                institute_name: "amala_convent_hr_sec_school",
+                year: 2017,
+                percentage: 89,
+
+            },
+            {
+                course_name: "Bsc.computerscience",
+                institute_name: "amala_convent_hr_sec_school",
+                year: 2020,
+                percentage: 91
+
+            }
+        ],
+    objective: "  A short statement that summarizes your career goals, skills, and experience, and why you're looking for a job. ",
+    skills: [
+        "html",
+        "css",
+        "boostrap"
+    ],
+    project:
+        [
+            {
+                project_name: "mini-project",
+                company_name: "DBMvircon",
+                duration:"2021-2022",
+                
+            }
+
+        ],
+    work_experience:
+        [
+            {
+                company_name: "DBMvircon",
+                experience: "2years",
+                year:"2021-2023"
+            }
+        ],
+    certification:
+        [
+            {
+            certification_name: "full stack web developer",
+            institute_name: "agaram academy",
+            place: " nagercoil",
+            year: 2024,
+            duration: "6_month"
+
+        }
+        ],
+    personal_details: {
+        father_name: "manikandan",
+        mother_name: "prema",
+        dob: "22-06-2000",
+        martiel_status: "married",
+        language_know: ["tamil", "english"],
+        notionality: "indian",
+        address: "2/206_kollenvilai,thuckalay"
+
+    },
+    declaration:
+        "i solemly conform all the information provided above is true to the best of a knowledge and ability ",
+    date: "26-11-24",
+    place: "nagercoil"
+}
+console.log(resume.name)
+
+
+
+
+
+
+
