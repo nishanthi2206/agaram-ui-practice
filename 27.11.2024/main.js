a3cdb85b7ab2ae8301bc04725c6bf39b42b46a30
nishanthi2206 <nishanthi.rn95@gmail.com>
2024-11-28 09:56:07 +0530

    update the function

--- 27.11.2024/main.js
+++ 27.11.2024/main.js
@@ -1,3 +1,89 @@
+function printmyname(){ 
+  console.log("nishanthi") 
+} 
+printmyname() 
+printmyname() 
+ 
+ 
+ 
+function printname(name){ 
+  console.log(name) 
+} 
+printname("nisha") 
+ 
+ 
+function addnumber(x,y){ 
+  let sum=x+y 
+  console.log(sum) 
+   
+} 
+addnumber(2,3) 
+addnumber(5,6) 
+ 
+function find(x,y,z,q){ 
+  let sum=x+y 
+  let multiply=z*q 
+  let subtract=sum-multiply 
+  console.log(subtract) 
+   
+} 
+find(1,2,3,4) 
+ 
+ 
+function fullname(x,y){ 
+let sum=x+y 
+console.log(sum) 
+} 
+fullname("agaram","academy") 
+ 
+let detail={ 
+  fname:"nishanthi", 
+  lname:"M" 
+} 
+function getfullname(object){ 
+  console.log(object.fname+object.lname) 
+} 
+getfullname(detail) 
+ 
+ 
+ 
+let name={ 
+  fname:"nishanthi", 
+  lname:"M" 
+} 
+let colors=["red","grn","yellow"]; 
+function getfullsentence(object,color) 
+{ 
+console.log(object.fname+object.lname+ " likes "+colors.length+" colors "+color) 
+} 
+getfullsentence(name,colors); 
+ 
+ 
+function addition(name1,name2) 
+{ 
+  return name1+name2; 
+} 
+let total1=addition(5,6); 
+let total2=addition(8,16); 
+console.log(total1+total2) 
+ 
+ 
+function addition(num1,num2) 
+{ 
+  return num1+num2; 
+} 
+function multiply(num1,num2) 
+{ 
+  return num1*num2; 
+} 
+function sub(num1,num2) 
+{ 
+  return num1-num2; 
+} 
+let solve1=addition(6,7); 
+let solve2=multiply(3,4); 
+let solve3=sub(solve1,solve2); 
+console.log(solve3)
 let mathsmark=55;
 if(mathsmark>60){
   console.log("you are pass")
@@ -8,7 +94,7 @@ else
   }
 let gender="female";
 if (gender=="female"){
-  console.log("color is pink")
+  console.log("color is pink")+
 }
 else if(gender=="male"){
   console.log("color is blue")
